a7e9b58b4e6bb38762345061d5a3092b4d5a2e46
Alodor <jose-a-mendez@hotmail.com>
2017-11-11 13:03:51 -0400

    Definiendo los metadatos en el head

---
+++ assets/head.php
@@ -0,0 +1,5 @@
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+<meta name="keywords" content="diseño web en venezuela, desarrollo de paginas web en venezuela, elaboración de sitios web en venezuela, diseño web y desarrollo de software en venezuela, alodor, desarrollo de app en venezuela, diseño web en anzoátegui, desarrollo de software a medida, comercio electrónico, tienda virtual, crear carrito de compra, diseño responsive, blog, php, larvel, aplicaciones web, como crear una tienda virtual">
+<meta name="description" content="Enfocado al diseño y desarrollo de aplicaciones web y moviles con estandares actuales de UI/UX, Seguridad y Compatibilidad con distintos navegadores y dispositivos.">        
+<link rel="shortcut icon" href="assets/images/favicon.ico">
---
+++ index.php
@@ -0,0 +1,10 @@
+<!Doctype html>
+<html lang="es">
+    <head>
+        <?php include 'assets/head.php'; ?>
+    </head>
+    
+    <body>
+    
+    </body>
+</html>
