deb1b15947b88cb301f59394f2159a8bd61d4d4b
Alodor <jose-a-mendez@hotmail.com>
2017-11-11 15:41:55 -0400

    Titluto del archivo index.php modificado

--- assets/head.php
+++ assets/head.php
@@ -3,4 +3,4 @@
 <meta name="keywords" content="diseño web en venezuela, desarrollo de paginas web en venezuela, elaboración de sitios web en venezuela, diseño web y desarrollo de software en venezuela, alodor, desarrollo de app en venezuela, diseño web en anzoátegui, desarrollo de software a medida, comercio electrónico, tienda virtual, crear carrito de compra, diseño responsive, blog, php, larvel, aplicaciones web, como crear una tienda virtual">
 <meta name="description" content="Enfocado al diseño y desarrollo de aplicaciones web y moviles con estandares actuales de UI/UX, Seguridad y Compatibilidad con distintos navegadores y dispositivos.">        
 <link rel="shortcut icon" href="assets/images/favicon.ico">
-<title>Alodor | Desarrollador Web</title>
+<title>Alodor | Desarrollador de aplicaciones</title>
