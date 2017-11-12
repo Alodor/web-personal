658d1924f2285ff4805f9ad8fc459d09970e53e4
Alodor <jose-a-mendez@hotmail.com>
2017-11-12 16:18:30 -0400

    Pagina de error terminada

--- error.php
+++ error.php
@@ -10,7 +10,40 @@
         <title>Alodor | Algo salió mal</title>    
     </head>
     
+    <style>
+        #error {
+            margin-top: 100px;
+            margin-bottom: 100px;
+        }
+    </style>
+    
     <body>
-        <h1>Hola Mundo</h1> 
+        <header>
+            <!-- Navbar -->
+            <?php include 'assets/navbar.php'; ?>
+            <!-- /Navbar -->
+        </header>
+       
+        <div id="error" class="container">
+            <div class="row">
+                <div class="col m6">
+                    <img class="responsive-img" src="assets/images/error.jpg">
+                </div>
+                
+                <div class="col m6 center">
+                    <h2>Oh, no...!!</h2>
+                    <p class="flow-text">La página que intenta visualizar no se encuentra disponible</p>
+                    <a class="waves-effect waves-light btn blue-grey darken-4" href="/">Volvamos al principio</a>
+                </div>
+            </div>
+        </div>
+        
+        <!-- Footer -->
+        <?php include 'assets/footer.php'; ?>
+        <!-- /Footer -->
+        
+        <!-- Scripts -->
+        <?php include 'assets/scripts.php'; ?>
+        <!-- /Scripts -->
     </body>
 </html>
