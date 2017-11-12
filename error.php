62f2a32a046f6bd579940c8cbd3d59d084e5d04b
Alodor <jose-a-mendez@hotmail.com>
2017-11-12 16:03:44 -0400

    Creando el archivo de error 404

---
+++ error.php
@@ -0,0 +1,16 @@
+<!Doctype html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
+        <link rel="shortcut icon" href="assets/images/favicon.ico">
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Oxygen:300,400,700">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
+        <link rel="stylesheet" href="assets/css/styles.css">
+        <title>Alodor | Algo salió mal</title>    
+    </head>
+    
+    <body>
+        <h1>Hola Mundo</h1> 
+    </body>
+</html>
