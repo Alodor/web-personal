5824cc77ab5e46721d7a81c2ee52d3e84300bb8a
Alodor <jose-a-mendez@hotmail.com>
2017-11-12 12:16:06 -0400

    Footer terminado

--- assets/footer.php
+++ assets/footer.php
@@ -1,29 +1,42 @@
-<section>
-    <footer class="page-footer">
+<footer class="page-footer">
+    <div class="container">
+        <div class="row">
+            <div class="col s12">
+                <h5>Encuéntrame en</h5>
+                <a href="https://www.facebook.com/alodor.com.ve/" target="_blank">
+                    <i class="col s3 col m1 fa fa-facebook"></i>
+                </a>
+                <a href="https://twitter.com/Alodor24" target="_blank">
+                    <i class="col s3 col m1 fa fa-twitter"></i>
+                </a>
+                <a href="https://www.youtube.com/channel/UCeveC_HlymoFVaRsWs2c5Pg/videos" target="_blank">
+                    <i class="col s3 col m1 fa fa-youtube-play"></i>
+                </a>
+                <a href="https://github.com/alodor" target="_blank">
+                    <i class="col s3 col m1 fa fa-github"></i>
+                </a>
+                <a href="https://www.linkedin.com/in/jos%C3%A9-alejandro-m%C3%A9ndez-s%C3%A1nchez-5046b082/" target="_blank">
+                    <i class="col s3 col m1 fa fa-linkedin"></i>
+                </a>
+                <a href="https://platzi.com/@alodor/" target="_blank">
+                    <i class="col s3 col m1 fa fa-gg"></i>
+                </a>
+                <a href="https://www.behance.net/jalejandrobf55" target="_blank">
+                    <i class="col s3 col m1 fa fa-behance"></i>
+                </a>
+                <a href="https://www.instagram.com/jalejandro.24/" target="_blank">
+                    <i class="col s3 col m1 fa fa-instagram"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="footer-copyright">
         <div class="container">
-            <div class="row">
-                <div class="col l6 s12">
-                    <h5 class="white-text">Footer Content</h5>
-                    <p class="grey-text text-lighten-4">You can use rows and columns here to organize your footer content.</p>
-                </div>
-      
-                <div class="col l4 offset-l2 s12">
-                    <h5 class="white-text">Links</h5>
-                    <ul>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 1</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 2</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 3</a></li>
-                        <li><a class="grey-text text-lighten-3" href="#!">Link 4</a></li>
-                    </ul>
-                </div>
-            </div>
+            © <?php echo date('Y'); ?> Copyright
+            <a class="right" href="/">
+                Desarrollado por Alodor 
+            </a>
         </div>
-  
-        <div class="footer-copyright">
-            <div class="container">
-                © 2014 Copyright Text
-                <a class="grey-text text-lighten-4 right" href="#!">More Links</a>
-            </div>
-        </div>
-    </footer>
-</section>
+    </div>
+</footer>
