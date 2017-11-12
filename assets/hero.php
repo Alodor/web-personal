82c363f9dcbb4e6d13a3ec0ab75943ad53e5765c
Alodor <jose-a-mendez@hotmail.com>
2017-11-11 22:39:42 -0400

    Inclusion de la seecion hero

--- assets/hero.php
+++ assets/hero.php
@@ -1,3 +1,9 @@
-<section>
-    
+<section class="hero-wrapper">
+    <div class="hero-filter">
+        <div class="container">
+            <h1 class="hero-title">José Alejandro Méndez</h1>
+            <h2 class="hero-subtitle">desarrollador freelance</h2>
+            <i id="scroll-down" class="material-icons">system_update_alt</i>
+        </div>
+    </div>
 </section>
