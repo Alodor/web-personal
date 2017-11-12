643865a2272dc4159ccd79260886bafac904969d
Alodor <jose-a-mendez@hotmail.com>
2017-11-11 23:08:39 -0400

    Definiendo las secciones internas

--- index.php
+++ index.php
@@ -15,6 +15,38 @@
             <!-- /Hero -->   
         </header>
         
+        <!-- Acerca de -->
+        <section class="section-wrapper section-light">
+            <div class="container">
+                <h3>Acerca de</h3>
+            </div>
+        </section>
+        <!-- /Acerca de -->
+        
+        <!-- Habilidades -->
+        <section class="section-wrapper section-light">
+            <div class="container">
+                <h3>Habilidades</h3>
+            </div>
+        </section>
+        <!-- /Habilidades -->
+        
+        <!-- Portafolio -->
+        <section class="section-wrapper section-grey">
+            <div class="container">
+                <h3>Portafolio</h3>
+            </div>
+        </section>
+        <!-- /Portafolio -->
+                
+        <!-- Contacto -->
+        <section class="section-wrapper section-light">
+            <div class="container">
+                <h3>Contacto</h3>
+            </div>
+        </section>
+        <!-- /Contacto -->
+        
         <!-- Footer -->
         <?php include 'assets/footer.php'; ?>
         <!-- /Footer -->
